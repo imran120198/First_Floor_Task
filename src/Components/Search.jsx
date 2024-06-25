76514e75368b9f02bf95d4476e59136baea88bea
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-06-25 18:04:00 +0530

    done

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -5,36 +5,41 @@ import { HiCurrencyRupee } from "react-icons/hi";
 
 const Search = () => {
   return (
-    <div className="bg-white m-auto w-[100%] h-[150px] md:w-3/4 lg:w-1/2 mt-8 p-4 rounded-lg shadow-lg flex items-center">
-      <div className="flex items-center w-full rounded-lg overflow-hidden">
-        <div className="flex items-center border border-black-800 rounded-lg ">
-          <FaLocationDot size={24} className="text-green-700 mx-2" />
-          <input
-            type="text"
-            placeholder="Location"
-            className="w-full p-2 text-lg focus:outline-none"
-          />
-        </div>
-        <div className="flex items-center border border-black-800 rounded-lg ">
-          <FaHouse size={24} className="text-green-700 mx-2" />
-          <input
-            type="text"
-            placeholder="Location"
-            className="w-full p-2 text-lg focus:outline-none"
-          />
-        </div>
-        <div className="flex items-center border border-black-800 rounded-lg ">
-          <HiCurrencyRupee size={24} className="text-green-700 mx-2" />
-          <input
-            type="text"
-            placeholder="Location"
-            className="w-full p-2 text-lg focus:outline-none"
-          />
-        </div>
+    <div className="bg-white m-auto w-[100%] h-[150px] md:w-3/4 lg:w-1/2 mt-8 p-4 rounded-lg shadow-lg flex flex-col space-y-4 z-50 opacity-120">
+      <div className="flex justify-around w-[150px] text-lg">
+        <p className="cursor-pointer">Buy</p>
+        <p className="cursor-pointer">Rent</p>
+        <p className="cursor-pointer">PGs</p>
       </div>
-      <button className="bg-blue-500 text-white p-2 flex items-center justify-center">
-        <FaSearch size={18} />
-      </button>
+      <div className="flex flex-col md:flex-row items-center w-full rounded-lg overflow-hidden space-y-4 md:space-y-0 md:space-x-4">
+        <div className="flex items-center border border-black-800 rounded-lg w-full md:w-auto">
+          <FaLocationDot size={20} className="text-green-700 mx-2" />
+          <input
+            type="text"
+            placeholder="Location"
+            className="w-full p-2 text-lg focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center border border-black-800 rounded-lg w-full md:w-auto">
+          <FaHouse size={20} className="text-green-700 mx-2" />
+          <input
+            type="text"
+            placeholder="Property Type"
+            className="w-full p-2 text-lg focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center border border-black-800 rounded-lg w-full md:w-auto">
+          <HiCurrencyRupee size={20} className="text-green-700 mx-2" />
+          <input
+            type="text"
+            placeholder="Budget"
+            className="w-full p-2 text-lg focus:outline-none"
+          />
+        </div>
+        <button className="bg-blue-500 text-white p-2 flex items-center justify-center">
+          <FaSearch size={20} />
+        </button>
+      </div>
     </div>
   );
 };
